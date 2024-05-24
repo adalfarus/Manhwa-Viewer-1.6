235e140404549f5e26ca9f7e0c90701dd84112dd
Adalfarus <51338419+adalfarus@users.noreply.github.com>
2024-05-24 15:20:06 +0200

    Reset

--- config.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-
-os.makedirs('./_internal/libs', exist_ok=True)
-sys.path.insert(0, './_internal/libs')
-del sys, os
--- nmv_compile.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call .\.venv\scripts\activate.bat
-python.exe -m pip install --upgrade pip
-pip install -r requirements.txt --upgrade
-auto-py-to-exe -c .\manhwaviewer-pyautoinst-config.json
